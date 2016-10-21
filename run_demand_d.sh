640f33a09c5e1671035ae09ea1b0d2b2c05fe093
liekkas <zjq0717@163.com>
2016-10-21 22:45:29 +0800

    use hdfs

--- run_demand_d.sh
+++ run_demand_d.sh
@@ -1,8 +1,8 @@
 /opt/spark/spark-1.6.2-bin-hadoop2.6/bin/spark-submit \
 --class com.citic.guoan.dvb.DemandByDay \
---master local[2] \
---jars /opt/jars/jedis-2.8.1.jar \
+--master yarn-cluster \
 /opt/jars/dvb-0.0.1-SNAPSHOT.jar \
-/opt/data/demodata \
-/opt/data/show_dict.txt \
+hdfs://10.91.33.17:9000/user/hadoop/zxga_data/demodata \
+hdfs://10.91.33.17:9000/user/hadoop/zxga_data/show_dict.txt \
+hdfs://10.91.33.17:9000/user/hadoop/zxga_data/demand_uid_count \
 /opt/data/result
