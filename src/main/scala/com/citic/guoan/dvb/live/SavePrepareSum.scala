93c19e9c764d4341c66cd24bcdd1252919bb15f5
liekkas <zjq0717@163.com>
2016-11-17 10:40:06 +0800

    save redis's data

---
+++ src/main/scala/com/citic/guoan/dvb/live/SavePrepareSum.scala
@@ -0,0 +1,24 @@
+package com.citic.guoan.dvb.live
+
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+import redis.clients.jedis.Jedis
+
+import scala.util.Sorting
+
+/**
+  * Created by liekkas on 16/11/16.
+  */
+object SavePrepareSum {
+  def main(args: Array[String]): Unit = {
+    val resultFile = new File("/Users/liekkas/IdeaProjects/asiainfo/scala/dvb/data/live/live_prepare_sum.txt")
+    val jedis = new Jedis("z1")
+    val result = jedis.smembers("RESULT").toArray.map(_.toString)
+    Sorting.quickSort(result)
+    result.foreach(row => {
+      println(row)
+      FileUtils.writeStringToFile(resultFile,row+"\n",true)
+    })
+  }
+}
