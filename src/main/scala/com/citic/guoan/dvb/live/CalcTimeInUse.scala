a20363aa309efe87f2a248efafc3bc57b5fbe4a1
liekkas <zjq0717@163.com>
2016-11-30 13:36:47 +0800

    combine the filter data and shutdown data to calc the real time-in-use

--- src/main/scala/com/citic/guoan/dvb/live/CalcTimeInUse.scala
+++ src/main/scala/com/citic/guoan/dvb/live/CalcTimeInUse.scala
@@ -3,14 +3,16 @@ package com.citic.guoan.dvb.live
 import java.util.Calendar
 
 import org.apache.commons.lang3.time.DateUtils
-import org.apache.hadoop.io.compress.{GzipCodec}
+import org.apache.hadoop.io.compress.GzipCodec
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by liekkas on 16/11/10.
   */
 object CalcTimeInUse {
+    case class LIVE(uid:String,date:String,channel_name:String,program_name:String)
     case class LEFT_DATA(row:Long,uid:String,month:Int,week:Int,day:String,hour:Int,
                          milliseconds:Long,channel_name:String,program_name:String)
     case class RIGHT_DATA(row:Long,uid:String,milliseconds:Long)
@@ -18,45 +20,49 @@ object CalcTimeInUse {
     def main(args: Array[String]): Unit = {
       val conf = new SparkConf().setAppName("CalcTimeInUse")
       conf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
-      conf.registerKryoClasses(Array(classOf[LEFT_DATA],classOf[RIGHT_DATA]))
+      conf.registerKryoClasses(Array(classOf[LIVE],classOf[LEFT_DATA],classOf[RIGHT_DATA]))
 
       val sc = new SparkContext(conf)
-      sc.setLogLevel("WARN")
       val sqlContext = new SQLContext(sc)
       import sqlContext.implicits._
 
-      //获取已排序好的数据
+      //获取过滤数据
       val origin = sc.textFile(args(0))
-      origin.cache()
+        .map(_.split("\t"))
+        .map(p => LIVE(p(0),p(1),p(2),p(3)))
+      //获取关机数据
+      val shutdown = sc.textFile(args(1))
+        .filter(_ != "")
+        .map(_.split("\t"))
+        .map(p => LIVE(p(0),p(1),p(2),p(3)))
 
-      //去掉每个分区的第一行数据,用于配对相减
-//      val pair = origin.mapPartitions(iter => iter.drop(1))
-      val first = origin.first()
-      val pair = origin.filter(_!=first)
+      val data = origin.union(shutdown)
+        .sortBy(p => p.uid + "," + p.date)
+      
+      data.persist(StorageLevel.MEMORY_AND_DISK_SER)
 
-      val leftT = origin.zipWithIndex()
+      val leftT = data.zipWithIndex()
         .map(p => {
-          val content = p._1.toString.split("\t")
+          val dateStr = p._1.date
           val index = p._2.toLong
-          val month = content(1).substring(0,6).toInt
-          val day = content(1).substring(0,4) + "-" + content(1).substring(4,6) + "-" + content(1).substring(6,8)
-          val hour = content(1).substring(8,10).toInt
+          val month = dateStr.substring(0,6).toInt
+          val day = dateStr.substring(0,4) + "-" + dateStr.substring(4,6) + "-" + dateStr.substring(6,8)
+          val hour = dateStr.substring(8,10).toInt
           val calendar = DateUtils.toCalendar(DateUtils.parseDate(day,"yyyy-MM-dd"))
           calendar.setMinimalDaysInFirstWeek(4) // For ISO 8601
           val weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR)
-          val week = content(1).substring(0,4) + (if(weekOfYear>9) weekOfYear else "0"+weekOfYear)
+          val week = dateStr.substring(0,4) + (if(weekOfYear>9) weekOfYear else "0"+weekOfYear)
           //绝对毫秒
-          val mis = DateUtils.parseDate(content(1), "yyyyMMddhhmmss").getTime
-          LEFT_DATA(index,content(0),month,week.toInt,day,hour,mis,content(2),content(3))
+          val mis = DateUtils.parseDate(dateStr, "yyyyMMddHHmmss").getTime
+          LEFT_DATA(index,p._1.uid,month,week.toInt,day,hour,mis,p._1.channel_name,p._1.program_name)
         }).toDF()
 
-      val rightT = pair.zipWithIndex()
+      val rightT = data.zipWithIndex()
         .map(p => {
-          val content = p._1.split("\t")
           val index = p._2.toLong
           //绝对毫秒
-          val mis = DateUtils.parseDate(content(1), "yyyyMMddhhmmss").getTime
-          RIGHT_DATA(index,content(0),mis)
+          val mis = DateUtils.parseDate(p._1.date, "yyyyMMddHHmmss").getTime
+          RIGHT_DATA(index-1,p._1.uid,mis) //数据错位
         }).toDF()
 
       leftT.registerTempTable("left_table")
@@ -74,9 +80,8 @@ object CalcTimeInUse {
            and a.uid = b.uid
         """.stripMargin)
 
-//      df.show()
-
-      df.filter(df("time_in_use") >= 5 && df("time_in_use") <= 36000)  //去掉小于5秒和大于10小时的数据
+      //去掉小于5秒和大于10小时的数据和关机数据
+      df.filter((df("channel_name") !== "_") && (df("time_in_use") >= 5 && df("time_in_use") <= 36000))
         .registerTempTable("filter_result")
 
       sqlContext.sql(
@@ -87,6 +92,10 @@ object CalcTimeInUse {
         """.stripMargin)
         .map(f => f(0) + "\t" +f(1) + "\t" +f(2) + "\t" +f(3) + "\t" +f(4) + "\t" +f(5) + "\t" +
           f(6) + "\t" + f(7).toString.toDouble.toInt)
-        .coalesce(args(2).toInt).saveAsTextFile(args(1), classOf[GzipCodec])
+        .coalesce(args(3).toInt)
+//        .saveAsTextFile(args(2))
+        .saveAsTextFile(args(2), classOf[GzipCodec])
     }
   }
+
+
