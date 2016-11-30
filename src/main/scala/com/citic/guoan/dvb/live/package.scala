1cfd87d0f1fa71f1bdae74c6a5b171233a6e5c4c
liekkas <zjq0717@163.com>
2016-11-30 14:53:30 +0800

    consts

---
+++ src/main/scala/com/citic/guoan/dvb/live/package.scala
@@ -0,0 +1,12 @@
+package com.citic.guoan.dvb
+
+/**
+  * Created by liekkas on 16/11/29.
+  */
+package object live {
+
+  val OPEN_MARKER = "0201|0999|10"
+  val HEARTBEAT_MARKER = "0201|0999|11"
+  val LIVE_MARKER = "0201|0999|01"
+  val DATE_FORMAT_STR = "yyyyMMddHHmmss"
+}
