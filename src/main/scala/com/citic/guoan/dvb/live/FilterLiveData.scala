2eae3218d6573ce52576987a4e7d31d7cb04a416
liekkas <zjq0717@163.com>
2016-11-30 13:32:41 +0800

    the first step:Filter

--- src/main/scala/com/citic/guoan/dvb/live/FilterLiveData.scala
+++ src/main/scala/com/citic/guoan/dvb/live/FilterLiveData.scala
@@ -7,10 +7,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Created by liekkas on 16/10/26.
   */
-object FilterData {
+object FilterLiveData {
   case class LIVE(uid:String,date:String,channel_name:String,program_name:String)
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("FilterData")
+    val conf = new SparkConf().setAppName("FilterLiveData")
     conf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
     conf.registerKryoClasses(Array(classOf[LIVE]))
     val sc = new SparkContext(conf)
@@ -19,7 +19,7 @@ object FilterData {
     //过滤掉日期格式不正常的(不足14位数),和用户id不正常的(不足6位数)
     //过滤掉日期不再20164月到9月之间的数据
     def filterFunc(line: Array[String]): Boolean = {
-      line.length >= 9 && line(0) == "0201|0999|01" && line(1) != "" && line(2) != "" &&
+      line.length == 11 && line(0) == LIVE_MARKER && line(1) != "" && line(2) != "" &&
         line(1).length == 14 && line(2).length > 6 &&
           line(1).substring(0,6).toInt > 201603 && line(1).substring(0,6).toInt < 201610
     }
@@ -34,9 +34,11 @@ object FilterData {
         val program = if(p(8)=="" || p(8)=="null") "?" else p(8)
         (p(2)+","+p(1),LIVE(p(2),p(1),channelFixed,program))
       })
-      .repartitionAndSortWithinPartitions(new ViewerIdPartitioner(args(2).toInt)) //重新分区并按UID和时间排序
+      //重新分区不排序 因为后面要和关机信息组合,还要进行一次排序,这里就不浪费时间了
+      .partitionBy(new ViewerIdPartitioner(args(2).toInt))
+//      .repartitionAndSortWithinPartitions(new ViewerIdPartitioner(args(2).toInt)) //重新分区并按UID和时间排序
       .map(f => f._2.uid + "\t" +f._2.date + "\t" +f._2.channel_name + "\t" +f._2.program_name)
+//      .saveAsTextFile(args(1))
       .saveAsTextFile(args(1),classOf[GzipCodec])
-//      .toDF().write.parquet(args(1)+"parquet")
   }
 }
